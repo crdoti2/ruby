ea5b8b7a9ba4ab7662bf767c6f85ca5b4b0f5e0f
y-furuya <katsu-goto@crdoyi.co.jp>
2016-04-13 16:55:23 +0900

    tab時間がなくて未完

--- furuya/tab.rb
+++ furuya/tab.rb
@@ -7,12 +7,11 @@ c=0
 a.each { |x|
 
 File.open(x) do |file|
-file.each_line do |y|
-c=c+y.count("	")
-    end
+
+c=c+file.read.count("	")
+   
   end
 
 puts File.basename(x).ljust(35) + c.to_s.rjust(10) 
-
 c=0
 }
